5cec938cca1db4df4e6dd0c6617370c61e9a8c1d
Dmitriy <dimonnikanorov2@gmail.com>
2022-11-02 14:55:18 +0200

    Create router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HandlerRouteError from './handlerRouteError'
+import Home from './Routes/home'
+import Squares from './Routes/squares'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <HandlerRouteError />
+  },
+  {
+    path: '/squares',
+    element: <Squares />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
